91522a6f9251bb6c5fb6a1a510193de0f6f450a7
fros-tech <fros@techcollege.dk>
2024-09-23 20:13:36 +0200

    Basic composition App

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    Home
+    <p>My name is {{ name }}, my age is {{ age }}</p>
+    <button @click="handleClick">Click Me! I Dare you!</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  setup() {
+    let name = 'Mario'  // not reactive values
+    let age = 30  // not reactive values as in data()
+
+    const handleClick = () => {
+      console.log('you clicked me')
+    }
+
+    return { name, age, handleClick }
   }
 }
 </script>
