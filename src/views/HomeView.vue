8037c0fc28caab77c64620ad8a93d4a4b5290b3a
fros-tech <fros@techcollege.dk>
2024-09-23 20:41:21 +0200

    Reactive values using refs

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,23 +3,31 @@
     Home
     <p>My name is {{ name }}, my age is {{ age }}</p>
     <button @click="handleClick">Click Me! I Dare you!</button>
+    <button @click="age++">Add one to age</button>
+    <input type="text" v-model="name">
   </div>
 </template>
 
 <script>
+import { ref } from 'vue';
+
 // @ is an alias to /src
 
 export default {
   name: 'HomeView',
   setup() {
-    let name = 'Mario'  // not reactive values
-    let age = 30  // not reactive values as in data()
+
+    // const p = ref(null) // instead of this.$ref
+    // let name='Mario'  // Will not be reactive
+    const name = ref('Mario')  // makes name reactive
+    const age = ref(30)
 
     const handleClick = () => {
-      console.log('you clicked me')
+      name.value = 'Luigi'
+      age.value=35
     }
 
-    return { name, age, handleClick }
+    return { name, age, handleClick}
   }
 }
 </script>
